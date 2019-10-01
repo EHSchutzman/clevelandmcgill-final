de6a72428c43a19b6dbf5f9215b6674070d3ead6
Ethan Schutzman <ehschutzman@wpi.edu>
2019-10-01 18:12:22 -0400

    Working experiment locally, need to expand for heroku

---
+++ public/js/experiment.js
@@ -0,0 +1,136 @@
+var images = new Array()
+
+function start_experiment(){
+
+    var experiment_div = document.getElementById('center_content')
+    console.log("found experiment div")
+    console.log(experiment_div)
+    experiment_div.style.backgroundColor = "white"
+    experiment_div.innerHTML = `<div id="title">
+    <h1>Please enter your information:</h1>
+        </div>
+        <div id="questionnaire">
+            <form action="" id="personal">
+                Name (Optional): <input type="text" id="name" name="name"></br>
+                Age: <input type="text" id="age" name="age"></br>
+                Gender: 
+                <label for="male">Male</label> <input type="radio" name="gender" id="gender" value="male">
+                <label for="female">Female</label> <input type="radio" name="gender" id="gender" value="female">
+                <label for="other">Prefer not to tell</label> <input type="radio" name="gender" id="gender" value="other"><br>
+                Country: <input type="text" id="country" name="country"></br>
+                Cat or Dog?
+                <label for="cat">Cat</label> <input type="radio" name="animal" id="animal" value="dog">
+                <label for="dog">Dog</label> <input type="radio" name="animal" id="animal" value="cat"><br>
+            </form>
+            <input onClick="submitDemographics()" type="submit" value="Submit">
+
+        </div>`
+    }
+
+    function submitDemographics(){
+        var formId = uuidv4();
+        var nameToSend = $('#name').val();
+        var ageToSend = $('#age').val();
+        var genderToSend = $("input[name='gender']:checked").val();
+        var countryToSend = $('#country').val();
+        var animalToSend = $("input[name='animal']:checked").val();
+      
+        var formData = {
+            "session_id": formId,
+            "name": nameToSend,
+            "age": ageToSend,
+            "gender": genderToSend,
+            "country": countryToSend,
+            "animal": animalToSend
+          }
+        localStorage.setItem("SessionInfo", JSON.stringify(formData))
+        console.log(formData)
+
+        var http = new XMLHttpRequest()
+        http.onreadystatechange = send_session_id
+        http.open("POST", "/sendSessionInfo")
+        http.send(localStorage.getItem("SessionInfo"))
+        
+    }
+
+    function send_session_id(){
+        if(this.readyState == 4 && this.status == 200 ){
+            beginExperiment()
+        }
+    }
+
+
+    function uuidv4() {
+        return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+          (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+        );
+      }
+
+    function beginExperiment(){
+        var experiment_div = document.getElementById('center_content')
+        console.log("here")
+        var http = new XMLHttpRequest()
+        http.onreadystatechange = getImageNames
+        http.open("GET", "/getImageNames")
+        http.send()
+
+
+    }
+    function getImageNames(){
+            if(this.readyState == 4 && this.status == 200 ){
+                var imageNames = JSON.parse(this.responseText)
+                localStorage.setItem("imageNames", JSON.stringify(imageNames))
+                advanceExperiment()
+            }
+    }
+
+    function advanceExperiment(){
+        var imageNames = JSON.parse(localStorage.getItem("imageNames"))
+        var num = randomIntFromInterval(0, imageNames.length - 1)
+        var experiment_div = document.getElementById('center_content')
+        experiment_div.style.backgroundColor = "white"
+        experiment_div.innerHTML = `<img id="experiment_image" src="`  + "/img/" + imageNames[num]+`  ">
+		<div>
+
+				What percentage is the smaller of the larger?: <input type="number" id="percentage"><br>
+				<input onClick="answerGraph()" type="submit" value="Submit">
+
+		</div>`
+        
+        imageNames.splice(num, 1)
+        if(imageNames.length == 0){
+            endExperiment()
+        }
+        localStorage.setItem('imageNames', JSON.stringify(imageNames))
+
+    }
+
+    function answerGraph(){
+        var image = document.getElementById("experiment_image").src.split("/").pop()
+        var answer = {
+            "image_name": image.split('.')[0],
+            "actual_answer": $("#percentage").val(),
+            "expected_answer": image.substring(0,2),
+            "session_id": JSON.parse(localStorage.getItem("SessionInfo")).session_id
+        }
+        var http = new XMLHttpRequest()
+        http.onreadystatechange = sendGraphAnswer
+
+        http.open("POST", "/sendAnswer")
+        http.send(JSON.stringify(answer))
+    }
+    
+    function sendGraphAnswer(){
+        if(this.readyState == 4 && this.status == 200 ){
+            console.log('werwe')
+            advanceExperiment()
+        }
+    }
+
+    function endExperiment(){
+        var experiment_div = document.getElementById('center_content')
+        experiment_div.innerHTML = `<h1>THANK YOU</h1>`
+    }
+    function randomIntFromInterval(min, max) { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
---
+++ server.js
@@ -0,0 +1,159 @@
+const express = require('express');
+const path = require('path');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+var admin = require('firebase-admin');
+var serviceAccount = require("../clevelandmcgill-c116c-firebase.json");
+var fs = require('fs')
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://clevelandmcgill-c116c.firebaseio.com"
+});
+var db = admin.database()
+
+const PORT = process.env.PORT || 5000;
+/*
+Sending data to the database:
+use the db object to set a reference.
+var ref = db.ref("restricted_access/secret_document"); -- this will be the "branch that data is being sent to"
+
+then call ref.set and pass the json object you want to send to the database.
+ref.set({
+    "alanisawesome": {
+      "date_of_birth": "June 23, 1912",
+      "full_name": "Alan Turing"
+    },
+    "gracehop": {
+      "date_of_birth": "December 9, 1906",
+      "full_name": "Grace Hopper"
+    }
+    });
+
+This will set data to be  "restricted_access/secret_document/alanisawesome" to have a "date_of_birth" and "full_name" fields
+and "restricted_access/secret_document/gracehop" to have a "date_of_birth" and "full_name" fields
+*/
+
+/*
+Retrieving Data from the database:
+    set a reference to the databse location you want to pull data from:
+    var ref = db.ref("server/saving-data/fireblog/posts")
+
+    You can then call ref.on() on the reference to retrieve data.
+
+    ref.on("value", function(snapshot) {
+        console.log(snapshot.val());
+      }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+      });
+
+
+*/
+
+/*
+    All documentation for the use of the firebase admin api is located at:
+
+    https://firebase.google.com/docs/database/admin/start
+    
+*/
+
+if (cluster.isMaster) {
+  console.error(`Node cluster master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+  });
+
+} else {
+  const app = express();
+  app.use(express.static('public'))
+  app.get('/', function(req, res) {
+      res.sendFile(path.join(__dirname + '/index.html'));
+
+  });
+
+  app.get('/getImageNames', function(req, res){
+    fs.readdir("public/img", function(err, items) {
+      res.send(JSON.stringify(items));
+   
+    });
+
+  })
+
+  app.post("/sendAnswer", function(req, res){
+    getReq(req).then(body =>{
+      //send info to /trials/sessionid/trial
+      var str = "Trials/" + body.session_id.toString() + "/" + body.image_name.toString()
+      var ref = db.ref(str)
+      ref.set(body)
+      res.sendStatus(200)
+    })
+
+  })
+  app.post("/sendSessionInfo", function(req, res){
+    getReq(req).then(body =>{
+      //send info to /Sessions/sessionid
+      var str = "Session/" + body.session_id.toString()
+      var ref = db.ref(str)
+      ref.set(body)
+      res.sendStatus(200)
+    })
+
+  })
+
+
+
+  app.listen(PORT, function () {
+    console.error(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
+  });
+
+}
+async function getReq(req) {
+  let body = []
+  return new Promise(function(resolve, reject) {
+    req.on('data', (chunk) => {
+      body.push(chunk)
+    }).on('end', () => {
+      body = Buffer.concat(body).toString()
+      resolve(JSON.parse(body))
+    })
+  })
+}
+
+  // Answer API requests
+
+/** FORM SUBMISSION AND STORAGE TO FIREBASE DATABASE **/
+/*
+var ref = db.ref("Session");
+
+$('#personal').submit(function(event) {
+  var formId = uuidv4();
+  var nameToSend = $('#name').val();
+  var ageToSend = $('#age').val();
+  var genderToSend = $("input[name='gender']:checked").val();
+  var countryToSend = $('#country').val();
+  var animalToSend = $("input[name='animal']:checked").val();
+
+  var formData = {
+    formId: {
+      "name": nameToSend,
+      "age": ageToSend,
+      "gender": genderToSend,
+      "country": countryToSend,
+      "animal": animalToSend
+    }
+  }
+
+  ref.set(formData);
+});
+
+function uuidv4() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
+*/
